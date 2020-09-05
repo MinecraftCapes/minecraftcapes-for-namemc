e537d70a66aa710ca5b80a58c7918f033be39559
James Harrison <james@fasttortoise.co.uk>
2020-09-05 23:52:46 +0100

    👩‍💻 Removed duplicate code

--- src/contentScript.js
+++ src/contentScript.js
@@ -6,84 +6,51 @@ fetch("https://minecraftcapes.net/profile/" + profileUuid).then(function(respons
     return response.json();
 }).then(function(body) {
     if(body.textures.ears != null) {
-        userHasEars();
+        userHasFeature("ears");
     }
 
     if(body.textures.cape != null) {
-        userHasCape();
+        userHasFeature("cape");
     }
 });
 
-function userHasCape() {
+function userHasFeature(type) {
     //Create the parent div
-    let capeDiv = document.createElement("div");
-    capeDiv.id = "minecraftcapes";
-    capeDiv.classList.add("card");
-    capeDiv.classList.add("mb-3");
+    let featureDiv = document.createElement("div");
+    featureDiv.id = "minecraftcapes-" + type;
+    featureDiv.classList.add("card");
+    featureDiv.classList.add("mb-3");
 
     //Add the title
-    let capeTitle = document.createElement("strong");
-    capeTitle.classList.add("card-header");
-    capeTitle.classList.add("py-1");
-    capeTitle.innerHTML = "<strong><a href=\"https://minecraftcapes.net\" target=\"_blank\" rel=\"nofollow noopener noreferrer\">MinecraftCapes Cape</a></strong>";
-    capeDiv.appendChild(capeTitle);
+    let featureTitle = document.createElement("strong");
+    featureTitle.classList.add("card-header");
+    featureTitle.classList.add("py-1");
+    featureTitle.innerHTML = "<strong><a href=\"https://minecraftcapes.net\" target=\"_blank\" rel=\"nofollow noopener noreferrer\">MinecraftCapes " + capitalizeFirstLetter(type)  + "</a></strong>";
+    featureDiv.appendChild(featureTitle);
 
     //Add the body
-    let capeBody = document.createElement("div");
-    capeBody.classList.add("card-body");
-    capeBody.classList.add("text-center");
-    capeDiv.appendChild(capeBody);
+    let featureBody = document.createElement("div");
+    featureBody.classList.add("card-body");
+    featureBody.classList.add("text-center");
+    featureDiv.appendChild(featureBody);
 
     //Add the image
-    let capeImage = document.createElement("img");
-    capeImage.src = "https://minecraftcapes.net/profile/" + profileUuid + "/cape";
-    capeImage.classList.add("img-fluid");
+    let featureImage = document.createElement("img");
+    featureImage.src = "https://minecraftcapes.net/profile/" + profileUuid + "/" + type;
+    featureImage.classList.add("img-fluid");
 
     //Puts the image in a href
-    let capeImageHref = document.createElement("a");
-    capeImageHref.href = "https://minecraftcapes.net/user/" + profileUuid + "/cape";
-    capeImageHref.target = "_blank";
-    capeImageHref.appendChild(capeImage);
-    capeBody.appendChild(capeImageHref);
+    let featureImageHref = document.createElement("a");
+    featureImageHref.href = "https://minecraftcapes.net/user/" + profileUuid + "/" + type;
+    featureImageHref.target = "_blank";
+    featureImageHref.appendChild(featureImage);
+    featureBody.appendChild(featureImageHref);
 
     //Insert the div
     let profileLeft = document.body.querySelector("main.container").querySelector(".row").querySelector("div.col-lg-4");
-    profileLeft.insertBefore(capeDiv, profileLeft.childNodes[7]);
+    profileLeft.insertBefore(featureDiv, profileLeft.childNodes[7]);
 }
 
-function userHasEars() {
-    //Create the parent div
-    let capeDiv = document.createElement("div");
-    capeDiv.id = "minecraftcapes";
-    capeDiv.classList.add("card");
-    capeDiv.classList.add("mb-3");
-
-    //Add the title
-    let capeTitle = document.createElement("strong");
-    capeTitle.classList.add("card-header");
-    capeTitle.classList.add("py-1");
-    capeTitle.innerHTML = "<strong><a href=\"https://minecraftcapes.net\" target=\"_blank\" rel=\"nofollow noopener noreferrer\">MinecraftCapes Ears</a></strong>";
-    capeDiv.appendChild(capeTitle);
-
-    //Add the body
-    let capeBody = document.createElement("div");
-    capeBody.classList.add("card-body");
-    capeBody.classList.add("text-center");
-    capeDiv.appendChild(capeBody);
-
-    //Add the image
-    let capeImage = document.createElement("img");
-    capeImage.src = "https://minecraftcapes.net/profile/" + profileUuid + "/ears";
-    capeImage.classList.add("img-fluid");
-
-    //Puts the image in a href
-    let capeImageHref = document.createElement("a");
-    capeImageHref.href = "https://minecraftcapes.net/user/" + profileUuid + "/ears";
-    capeImageHref.target = "_blank";
-    capeImageHref.appendChild(capeImage);
-    capeBody.appendChild(capeImageHref);
-
-    //Insert the div
-    let profileLeft = document.body.querySelector("main.container").querySelector(".row").querySelector("div.col-lg-4");
-    profileLeft.insertBefore(capeDiv, profileLeft.childNodes[7]);
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
